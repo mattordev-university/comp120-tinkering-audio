690c3b2c82088e1a31dd373c607c1b5aef48eaba
Matthew Roberts <MR232432@falmouth.ac.uk>
2020-11-17 12:45:17 +0000

    Added some basic UI, and imported some code from the workshop for changing

--- SoundEffectGenerator/SoundEffectGenerator/Form1.Designer.cs
+++ SoundEffectGenerator/SoundEffectGenerator/Form1.Designer.cs
@@ -28,13 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sound Effect Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Sound";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save Sound";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(19, 61);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(601, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(626, 61);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(626, 84);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(626, 107);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(712, 107);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(712, 84);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(712, 61);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 7;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
     }
 }
 
--- SoundEffectGenerator/SoundEffectGenerator/Form1.cs
+++ SoundEffectGenerator/SoundEffectGenerator/Form1.cs
@@ -3,18 +3,217 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using NAudio;
+using NAudio.Wave;
 
 namespace SoundEffectGenerator
 {
+
     public partial class Form1 : Form
     {
+
+        private readonly int SAMPLE_RATE = 44100;
+        private readonly int CHANNEL_COUNT = 1;
+        private readonly int MAX_VALUE = (int)Math.Pow(2, 15);
+        private readonly int DEFAULT_MELODY_NOTE_COUNT = 12;
+
+        private readonly double volume = 0.08;
+
+        private delegate double WaveFunction(double frequency, int position);
+        private delegate double MathFunction(double x);
+
+        private WaveOut waveOut = null;
+
+        private List<double> notes;
+        private double[] noteDurations;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<int> GenerateSilence(double durationInSeconds)
+        {
+            List<int> silence = new List<int>();
+
+            for (int i = 0; i < (int)(durationInSeconds * SAMPLE_RATE); i++)
+            {
+                silence.Add(0);
+            }
+
+            return silence;
+        }
+
+        private List<int> GenerateTone(double durationInSeconds, WaveFunction waveFunction, double[] frequencies)
+        {
+            List<int> tone = new List<int>();
+
+            short value;
+            for (int i = 0; i < (int)(durationInSeconds * SAMPLE_RATE); i++)
+            {
+                value = 0;
+                for (int j = 0; j < frequencies.Length; j++)
+                {
+                    value += (short)(waveFunction.Invoke(frequencies[j], i));
+                }
+                tone.Add(value);
+            }
+            return tone;
+        }
+
+        private List<int> GenerateRandomMelody(int countOfNotesToPlay, WaveFunction waveFunction)
+        {
+            Random rng = new Random();
+
+            List<int> melody = new List<int>();
+            melody.AddRange(GenerateSilence(0.1));
+
+            double[] frequencies = new double[2];
+            for (int i = 0; i < countOfNotesToPlay; i++)
+            {
+                frequencies[0] = GetRandomElement(notes, rng);
+                frequencies[1] = GetRandomElement(notes, rng);
+
+                melody.AddRange(
+                    GenerateTone(
+                        GetRandomElement<double>(noteDurations, rng),
+                        waveFunction,
+                        frequencies
+                    ));
+            }
+
+            return melody;
+        }
+
+        private static T GetRandomElement<T>(IEnumerable<T> enumerable, Random rng)
+        {
+            int index = rng.Next(0, enumerable.Count());
+            return enumerable.ElementAt(index);
+        }
+
+        /// <summary>
+        /// Populates a given list of doubles with frequencies representing notes
+        /// according to Helmholtz’s scale . Will clear the list prior to populating.
+        /// </summary>
+        /// <param name="notes">
+        /// The list of notes which will be populated</param>
+        /// <param name="baseNote">Base note for tuning an equal−tempered scale, defaults to A4 (440Hz)
+        /// </param>
+        /// <param name="startNote">
+        /// Distance from baseNote to start, typically negative or zero
+        /// </param>
+        /// <param name="endNote">
+        /// Distance from baseNote to end, typically positive, must be larger than startNote
+        /// </param>
+        /// <param name="increment">
+        /// Interval between notes,typically 1
+        /// </param>
+        private List<double> PopulateNotes(double baseNote, int startNote, int endNote, int increment)
+        {
+            double ESTIMATOR = Math.Pow(2.0, (1.0 / 12.0));
+
+            List<double> notes = new List<double>();
+
+            for (int i = startNote; i < endNote; i += increment)
+            {
+                notes.Add(baseNote * Math.Pow(ESTIMATOR, i));
+            }
+
+            return notes;
+        }
+
+        private double SquareWave(double frequency, int position)
+        {
+            double value = PositionInWavePeriod(frequency, position, Math.Sin, 2.0);
+            if (value > 0)
+            {
+                value = 1.0;
+            }
+            else
+            {
+                value = -1.0;
+            }
+            return MAX_VALUE * volume * value;
+        }
+
+        private double SinWave(double frequency, int position)
+        {
+            return MAX_VALUE * volume * PositionInWavePeriod(frequency, position, Math.Sin, 2.0);
+        }
+
+        private double PositionInWavePeriod(double frequency, int position, MathFunction function, double piMultiplier)
+        {
+            return function.Invoke(piMultiplier * Math.PI * frequency * (position / (double)SAMPLE_RATE));
+        }
+
+        private void TinkeringAudioForm_Load(object sender, EventArgs e)
+        {
+            notes = PopulateNotes(440, -16, 8, 2);
+            noteDurations = new double[] { 0.5, 0.2, 0.3, 0.4 };
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            waveOut = new WaveOut();
+            waveOut.Init(convertToWaveProvider16(GenerateRandomMelody(DEFAULT_MELODY_NOTE_COUNT, SinWave), SAMPLE_RATE, CHANNEL_COUNT));
+            waveOut.Play();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+            string filename = Path.Combine(
+                Path.GetTempPath(),
+                Guid.NewGuid().ToString() + ".wav"
+                );
+
+            WaveFileWriter.CreateWaveFile(filename, convertToWaveProvider16(GenerateRandomMelody(DEFAULT_MELODY_NOTE_COUNT, SinWave), SAMPLE_RATE, CHANNEL_COUNT));
+        }
+
+        private IWaveProvider convertToWaveProvider16(List<int> sample, int sampleRate, int channelCount)
+        {
+            // Buffer is doubled for short -> byte conversion
+            byte[] byteBuffer = new byte[sample.Count * 2];
+
+            int byteArrayIndex = 0;
+            short value;
+
+            for (int i = 0; i < sample.Count; i++)
+            {
+                if (sample[i] > MAX_VALUE)
+                {
+                    value = (short)MAX_VALUE;
+                }
+                else if (sample[i] < -MAX_VALUE)
+                {
+                    value = (short)-MAX_VALUE;
+                }
+                else
+                {
+                    value = (short)sample[i];
+                }
+
+                byteBuffer[byteArrayIndex++] = BitConverter.GetBytes(value)[0];
+                byteBuffer[byteArrayIndex++] = BitConverter.GetBytes(value)[1];
+            }
+
+            IWaveProvider waveProvider = new RawSourceWaveStream(
+                new MemoryStream(byteBuffer),
+                new WaveFormat(sampleRate, 16, channelCount)
+                );
+
+            return waveProvider;
         }
     }
 }
